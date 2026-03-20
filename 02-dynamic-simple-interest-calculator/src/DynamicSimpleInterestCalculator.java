a34e64d01b5292a17741ef750ea78f638e040823
Sagar Maurya <sagarmaurya010@gmail.com>
2026-03-20 19:46:45 +0530

    Add 02-dynamic-simple-interest-calculator

--- 02-dynamic-simple-interest-calculator/src/DynamicSimpleInterestCalculator.java
+++ 02-dynamic-simple-interest-calculator/src/DynamicSimpleInterestCalculator.java
@@ -16,8 +16,56 @@ public class DynamicSimpleInterestCalculator {
 
         switch(choice) {
             case 1:
-                System.out.println("Enter Principal Amount (P): ");
+                System.out.print("Enter Principal (P): ");
                 P = sc.nextDouble();
+                System.out.print("Enter Rate (R): ");
+                R = sc.nextDouble();
+                System.out.print("Enter Time (T): ");
+                T = sc.nextDouble();
+
+                SI = (P * R * T) / 100;
+                System.out.print("Simple Interest (SI) = " + SI);
+                break;
+
+            case 2:
+                System.out.print("Enter Simple Interest (SI): ");
+                SI = sc.nextDouble();
+                System.out.print("Enter Rate (R): ");
+                R = sc.nextDouble();
+                System.out.print("Enter Time (T): ");
+                T = sc.nextDouble();
+
+                P = (SI * 100) / (R * T);
+                System.out.println("Principal Amount (P) = " + P);
+                break;
+
+            case 3:
+                System.out.print("Enter Simple Interest (SI): ");
+                SI = sc.nextDouble();
+                System.out.print("Enter Principal (P): ");
+                P = sc.nextDouble();
+                System.out.print("Enter Time (T): ");
+                T = sc.nextDouble();
+
+                R = (SI * 100) / (P * T);
+                System.out.println("Rate of Interest (R) = " + R);
+                break;
+
+            case 4:
+                System.out.print("Enter SI: ");
+                SI = sc.nextDouble();
+                System.out.print("Enter Principal: ");
+                P = sc.nextDouble();
+                System.out.print("Enter Rate: ");
+                R = sc.nextDouble();
+
+                T = (SI * 100) / (P * R);
+                System.out.println("Time = " + T);
+                break;
+
+            default:
+                System.out.println("Invalid choice!");
         }
+        sc.close();
     }
 }
